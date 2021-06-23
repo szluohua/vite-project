b7a85beec6e354aa980ce7f90f9a2dab83d742d9
leo <leo@luo-2.local>
2021-06-23 21:43:27 +0800

    init project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  // create naive ui
+  create,
+  // component
+  NButton,
+} from "naive-ui";
+
+const naive = create({
+  components: [NButton],
+});
+const app = createApp(App);
+app.use(naive);
+app.mount("#app");
